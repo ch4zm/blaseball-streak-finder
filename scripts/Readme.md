d43c8f7d2ad9e2446e500b6f1994fdc2950f899f
Ch4zm of Hellmouth <ch4zm.of.hellmouth@gmail.com>
2020-09-14 15:34:35 -0700

    update scripts

--- scripts/Readme.md
+++ scripts/Readme.md
@@ -1,12 +1,31 @@
 # `bump_version_number.py`
 
 This program will parse the `__init__.py` file of the project and
-increment the `__version__` variable by the smallest amount possible
-(so 0.5 will become 0.6, 0.5.1 will become 0.5.2, etc.)
+increment the `__version__` variable by a major, minor, or patch
+amount:
 
-# `deploy_minor_version.sh`
+```
+bump_version_number.py --major
+bump_version_number.py --minor
+bump_version_number.py --patch
+```
+
+Major will update the leading digit of the version number: 0.5 -> 1.0, 1.1.1 -> 2.0.0
+
+Minor will update the middle digit of the version number: 0.2.5 -> 0.3.0, 1.1.1 -> 1.2.0
+
+Patch will update the last digit of the version number: 0.2.5 -> 0.2.6, 1.1.1 -> 1.1.2
+
+# `deploy_new_version.sh`
 
 This script creates a virtual Python environment, installs the package
 and its dependencies into it, and runs through all of the steps to build,
 check, install, and upload the next version of the software.
 
+Pass a flag to indicate whether it should update the major, minor, or patch version:
+
+```
+deploy_new_version.sh --major
+deploy_new_version.sh --minor
+deploy_new_version.sh --patch
+```
--- scripts/bump_version_number.py
+++ scripts/bump_version_number.py
@@ -1,32 +1,87 @@
+import sys
 import os
 import re
+import argparse
 
 """
-This script bumps the version number in the most minor way possible.
+This script bumps the minor version number.
 If your version number is 0.1, it will bump it to 0.2.
 If your version number is 0.0.1, it will bump it to 0.0.2.
-For anything more major, do it yourself!
 """
 
-root_path = os.path.abspath(os.path.join(os.path.dirname(__file__)))
-init_path = os.path.abspath(os.path.join(root_path, '..', 'streak_finder'))
-INIT = "__init__.py"
 
-with open(os.path.join(init_path, INIT), 'r') as f:
-    content = f.read()
+pkg_name = 'interesting_blaseball_games'
 
-lines = content.split("\n")
-for line in lines:
-    m = re.search(r'__version__ = "(.*)"', line.strip())
-    if m is not None:
-        version_str = m.group(1)
-        versions = [int(j) for j in version_str.split(".")]
-        versions[-1] += 1
-        new_version_str = ".".join([str(j) for j in versions])
-        content = re.sub(version_str, new_version_str, content)
-        break
 
-with open(os.path.join(init_path, INIT), 'w') as f:
-    f.write(content)
+def usage():
+    print("")
+    print("Bump version number script:")
+    print("")
+    print("Usage:")
+    print("    python bump_version_number.py [--major,--minor]")
+    print("")
+    print("Use the --minor flag to bump the minor version (0.1 -> 0.2, 1.5.7 -> 1.5.8, etc)")
+    print("Use the --major flag to bump the major version (0.1 -> 1.0, 1.5.7 -> 2.0.0, etc)")
+    print("")
+    sys.exit(1)
 
-print(new_version_str)
+def main(args):
+    root_path = os.path.abspath(os.path.join(os.path.dirname(__file__)))
+    init_path = os.path.abspath(os.path.join(root_path, '..', pkg_name))
+    INIT = "__init__.py"
+    
+    with open(os.path.join(init_path, INIT), 'r') as f:
+        content = f.read()
+    
+    lines = content.split("\n")
+    for line in lines:
+        m = re.search(r'__version__ = "(.*)"', line.strip())
+        if m is not None:
+            version_str = m.group(1)
+            versions = [int(j) for j in version_str.split(".")]
+
+            if args.major:
+                versions[0] += 1
+                for i in range(1,len(versions)):
+                    versions[i] = 0
+            elif args.minor:
+                versions[1] += 1 
+                if len(versions)>2:
+                    for i in range(2, len(versions)):
+                        versions[i] = 0
+            elif args.patch:
+                versions[-1] += 1
+            new_version_str = ".".join([str(j) for j in versions])
+            content = re.sub(version_str, new_version_str, content)
+            break
+    
+    with open(os.path.join(init_path, INIT), 'w') as f:
+        f.write(content)
+    
+    print(new_version_str)
+
+if __name__=="__main__":
+
+    p = argparse.ArgumentParser()
+    g = p.add_mutually_exclusive_group()
+    g.add_argument('--patch',
+                   required=False,
+                   default=False,
+                   action='store_true',
+                   help='Bump the patch version (0.1.0 -> 0.1.1, 1.5.7 -> 1.5.8, etc)')
+    g.add_argument('--minor',
+                   required=False,
+                   default=False,
+                   action='store_true',
+                   help='Bump the minor version (0.1 -> 0.2, 1.5.7 -> 1.6.0, etc)')
+    g.add_argument('--major',
+                   required=False,
+                   default=False,
+                   action='store_true',
+                   help='Bump the major version (0.1 -> 1.0, 1.5.7 -> 2.0.0, etc)')
+
+    args = p.parse_args(sys.argv[1:])
+    if not args.patch and not args.minor and not args.major:
+        usage()
+
+    main(args)
